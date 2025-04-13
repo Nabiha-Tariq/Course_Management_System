0520795dd55b577d0d4e7100f46add2dc023c5d9
Nabiha <nabihatariq2005@gmail.com>
2025-04-13 20:16:31 +0500

    Resolved conflicts and updated authentication logic

--- backend/routes/Authentication.js
+++ backend/routes/Authentication.js
@@ -1,27 +1,118 @@
 const express = require('express');
 const Admin = require('../models/Admin');
+const Student = require('../models/Student');
+const Teacher = require('../models/Teacher');
 const router = express.Router();
 const { body, validationResult } = require('express-validator');
 
 
-//Create a User using POST "/api/Authentication"
-router.post('/',[
+//Create a User using POST "/api/Authentication/createAdmin" .No login required
+router.post('/createAdmin',[
     body('firstName','Enter a valid name').isLength({min: 3}),
     body('lastName','Enter a valid last name').isLength({min: 3}),
     body('email','Enter a valid email').isEmail(),
     body('password','Password must atleast 5 characheters').isLength({min: 5})
 ],async (req, res) => {
+  // if there are errors return bad request and the errors
     const errors= validationResult(req);
    if (!errors.isEmpty()) {
       res.status(400).json({ errors:errors.array() });
     }
-    await Admin.create({
+
+    // check the whether admin with this email exists already
+
+    try{
+
+    let admin =await Admin.findOne({email: req.body.email});
+    if(admin){
+      return res.status(400).json({errors:"Sorry a admin with this email have already exist"})
+    }
+    admin= await Admin.create({
         firstName: req.body.firstName,
         lastName: req.body.lastName,
         email: req.body.email,
         password: req.body.password,
         status: req.body.status
     })
+
+    res.json({"message":"save successfully"})
+  }catch(error){
+    console.error(error.message)
+    res.status(500).send("some error occur")
+  }
   });
+
+//Create a User using POST "/api/Authentication/createStudent" .No login required
+router.post('/createStudent',[
+  body('firstName','Enter a valid name').isLength({min: 3}),
+  body('lastName','Enter a valid last name').isLength({min: 3}),
+  body('email','Enter a valid email').isEmail(),
+  body('password','Password must atleast 5 characheters').isLength({min: 5})
+],async (req, res) => {
+// if there are errors return bad request and the errors
+  const errors= validationResult(req);
+ if (!errors.isEmpty()) {
+    res.status(400).json({ errors:errors.array() });
+  }
+
+  // check the whether student with this email exists already
+
+  try{
+
+  let student =await Student.findOne({email: req.body.email});
+  if(student){
+    return res.status(400).json({errors:"Sorry a admin with this email have already exist"})
+  }
+  student= await Student.create({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      password: req.body.password,
+      status: req.body.status
+  })
+
+  res.json({"message":"save successfully"})
+}catch(error){
+  console.error(error.message)
+  res.status(500).send("some error occur")
+}
+});
+
+
+//Create a User using POST "/api/Authentication/createTeacher" .No login required
+router.post('/createTeacher',[
+  body('firstName','Enter a valid name').isLength({min: 3}),
+  body('lastName','Enter a valid last name').isLength({min: 3}),
+  body('email','Enter a valid email').isEmail(),
+  body('password','Password must atleast 5 characheters').isLength({min: 5})
+],async (req, res) => {
+// if there are errors return bad request and the errors
+  const errors= validationResult(req);
+ if (!errors.isEmpty()) {
+    res.status(400).json({ errors:errors.array() });
+  }
+
+  // check the whether student with this email exists already
+
+  try{
+
+  let teacher =await Teacher.findOne({email: req.body.email});
+  if(teacher){
+    return res.status(400).json({errors:"Sorry a admin with this email have already exist"})
+  }
+  teacher= await Teacher.create({
+      firstName: req.body.firstName,
+      lastName: req.body.lastName,
+      email: req.body.email,
+      password: req.body.password,
+      status: req.body.status
+  })
+
+  res.json({"message":"save successfully"})
+}catch(error){
+  console.error(error.message)
+  res.status(500).send("some error occur")
+}
+});
 
 module.exports = router
